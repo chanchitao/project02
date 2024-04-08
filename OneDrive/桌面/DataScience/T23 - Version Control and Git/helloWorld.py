1285d669f1f16618075ac3ac19b1b0de2c1c890f
chanchitao <164165963+chanchitao@users.noreply.github.com>
2024-04-08 22:23:46 +0100

    Update helloWorld.py
    change input with variables
    add comment

--- OneDrive/桌面/DataScience/T23 - Version Control and Git/helloWorld.py
+++ OneDrive/桌面/DataScience/T23 - Version Control and Git/helloWorld.py
@@ -1,13 +1,22 @@
+print("Git is awesome!")
+### start to accept from user, user has to input first name , surname
 print('Enter your first name:')
-firstname = input()
+### assign the input of the first name to variable x
+x = input()
 print('Enter your Surname:')
-surname = input()
+y = input()
 print('Enter your mobile no.')
-mobileNo = input() 
+z = input() 
 print('Enter your post code.')
 postcode = input() 
+### Input City
 print('Enter your City.')
 City = input() 
 ## Input Address 
 print('Enter your Address.')
 Address = input() 
+### Print out user input
+print('Hello, ' + x + " "+ y)
+print('Your Mobile No. is ' + z)
+print('Your Post Code is ' + postcode)
+print('You are living at ' + Address + " "+ City )
